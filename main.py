1f5f79bc0976a207f14dcf0f2493a3bd7726fe77
Jakub515 <jakub515.szczurek@gmail.com>
2025-10-26 15:26:55 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import load_images
+import time
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+
+image_load = load_images.ImageLoad()
+
+time.sleep(5)   
